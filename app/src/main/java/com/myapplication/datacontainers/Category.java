25a5bc780f1242e0db8ffdfbe8046d65e2551dbb
Domenick Casper <domenicktcasper@gmail.com>
2019-03-20 13:48:01 -0600

    added JavaDocs file, and added some comments

--- app/src/main/java/com/myapplication/datacontainers/Category.java
+++ app/src/main/java/com/myapplication/datacontainers/Category.java
@@ -6,14 +6,26 @@ public class Category {
     private String name;
     private ArrayList<Item> items = new ArrayList<>();
 
+    /**
+     * Creates a category with the name that is passed into it.
+     * @param name The name of the category.
+     */
     public Category(String name) {
         this.name = name;
     }
 
+    /**
+     * A good old getter for the name.
+     * @return the name specified.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * A good old setter for the name, so the category can have a name.
+     * @param name sets the name you specify it!
+     */
     public void setName(String name) {
         this.name = name;
     }
