95db3b8519dac7397d2922d25614db1f8fa9cf31
Domenick Casper <domenicktcasper@gmail.com>
2019-04-01 18:58:09 -0600

    Tests exist, fixed issues(ish)

--- app/src/test/java/com/tallyup/TestInventory.java
+++ app/src/test/java/com/tallyup/TestInventory.java
@@ -1,6 +1,5 @@
 package com.tallyup;
 
-import com.tallyup.datacontainers.Category;
 import com.tallyup.datacontainers.Inventory;
 import com.tallyup.datacontainers.Item;
 
@@ -10,11 +9,44 @@ import static junit.framework.TestCase.assertEquals;
 
 public class TestInventory {
     @Test
-    public void TestGetSetItem() {
+    public void AddCrapTonOfItems() {
         Inventory inventory = new Inventory();
         for (int i = 0; i < 1500; i++) {
             inventory.addItem("Item","Category #" + Integer.toString(i),1);
         }
         assertEquals(inventory.getCategoriesList().size(), 1500);
+    }
+
+    @Test
+    public void QuantityOverlap() {
+        Inventory inventory = new Inventory();
+        for (int i = 0; i < 1500; i++) {
+            inventory.addItem("Item","Category",1);
+        }
+        assertEquals(inventory.getItem("Category", "Item").getQuantity(), 1500);
+    }
+
+    @Test
+    public void MoveItems() {
+        Inventory inventory = new Inventory();
+        Inventory sold = new Inventory();
+        inventory.addItem("Item","Category",50);
+        Item i = inventory.getItem("Category", "Item");
+        sold.addItem("Item", "Category", 10);
+        i.setQuantity(i.getQuantity() - 10);
+        sold.sortItems();
+
+        assertEquals(inventory.getItem("Category", "Item").getQuantity(), 40);
+        assertEquals(sold.getItem("Category", "Item").getQuantity(), 10);
+    }
+
+    @Test
+    public void TestSorting() {
+        Inventory inventory = new Inventory();
+        inventory.addItem("B", "Category", 15);
+        inventory.addItem("A","Category",10);
+        inventory.sortItems();
+
+        assertEquals(inventory.getCategory("Category").getItems().get(0).getName(), "A");
     }
 }
